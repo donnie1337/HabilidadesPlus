802d04fb7011fbdbf1644c9d696d3d75f3ca6bd9
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-22 12:37:32 -0300

    fix: corrige pacote do BrewEvent

--- src/main/java/com/rpgcustom/habilidadesplus/listeners/AlchemyListener.java
+++ src/main/java/com/rpgcustom/habilidadesplus/listeners/AlchemyListener.java
@@ -9,7 +9,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BrewEvent;
+import org.bukkit.event.inventory.BrewEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryType;
