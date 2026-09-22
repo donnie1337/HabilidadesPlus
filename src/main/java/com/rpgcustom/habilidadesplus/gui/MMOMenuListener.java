37bd413e3e0a1ca12a0758b93c84a335ad14a927
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-22 14:59:38 -0300

    Corrige seta de voltar das habilidades

--- src/main/java/com/rpgcustom/habilidadesplus/gui/MMOMenuListener.java
+++ src/main/java/com/rpgcustom/habilidadesplus/gui/MMOMenuListener.java
@@ -44,7 +44,7 @@ public final class MMOMenuListener implements Listener {
             return;
         }
 
-        if (event.getRawSlot() == 45) {
+        if (event.getRawSlot() == 31) {
             MMOMenu.open(player, dataManager, levelingManager, configManager);
         }
     }
