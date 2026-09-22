c295cb2ef3482df52863711a8ce6345938fa0ad3
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-22 14:12:51 -0300

    Define Super Quebrador para clique direito

--- src/main/java/com/rpgcustom/habilidadesplus/abilities/SuperBreakerManager.java
+++ src/main/java/com/rpgcustom/habilidadesplus/abilities/SuperBreakerManager.java
@@ -46,8 +46,7 @@ public class SuperBreakerManager implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onInteract(PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK
-                && event.getAction() != Action.LEFT_CLICK_BLOCK) {
+        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) {
             return;
         }
 
