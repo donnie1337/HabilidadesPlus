745ada06ad928880eeb1f8bc50d65e0fff4c983e
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-22 16:59:43 -0300

    Adiciona estado de menu de ranking

--- src/main/java/com/rpgcustom/habilidadesplus/gui/MMOMenuHolder.java
+++ src/main/java/com/rpgcustom/habilidadesplus/gui/MMOMenuHolder.java
@@ -5,9 +5,17 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
 public final class MMOMenuHolder implements InventoryHolder {
-    private Inventory inventory; private final SkillType skill;
-    public MMOMenuHolder(SkillType skill){this.skill=skill;}
-    public SkillType skill(){return skill;}
-    @Override public Inventory getInventory(){return inventory;}
-    public void setInventory(Inventory inventory){this.inventory=inventory;}
+    private Inventory inventory;
+    private final SkillType skill;
+    private final boolean ranking;
+
+    public MMOMenuHolder(SkillType skill) { this(skill, false); }
+    public MMOMenuHolder(SkillType skill, boolean ranking) {
+        this.skill = skill;
+        this.ranking = ranking;
+    }
+    public SkillType skill() { return skill; }
+    public boolean ranking() { return ranking; }
+    @Override public Inventory getInventory() { return inventory; }
+    public void setInventory(Inventory inventory) { this.inventory = inventory; }
 }
