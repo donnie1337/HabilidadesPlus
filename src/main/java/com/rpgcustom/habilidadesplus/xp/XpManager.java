aaf24db4579d51f74a121477f06f2cc012a76bf3
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-23 11:21:25 -0300

    Corrigir imports da cor do cargo

--- src/main/java/com/rpgcustom/habilidadesplus/xp/XpManager.java
+++ src/main/java/com/rpgcustom/habilidadesplus/xp/XpManager.java
@@ -14,9 +14,11 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.Registry;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
