d5c6a363008367ed03845adc1c9ce88f3e1b4811
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-24 13:59:04 -0300

    style(gui): use green names for unlocked skills

--- src/main/java/com/rpgcustom/habilidadesplus/gui/MMOMenu.java
+++ src/main/java/com/rpgcustom/habilidadesplus/gui/MMOMenu.java
@@ -462,7 +462,7 @@ public final class MMOMenu {
         }
 
         lore.add("");
-        String color = !power.implemented() ? "&8" : unlocked ? "&b" : "&8";
+        String color = !power.implemented() ? "&8" : unlocked ? "&a" : "&8";
         return item(power.icon(), color + power.name() + " &8(" + skill.getDisplayName() + ")", lore);
     }
 
