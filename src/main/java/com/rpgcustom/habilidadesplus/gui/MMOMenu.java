c3d1adf648a606b30bded02f7dc1b7eb5ed6d9ee
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-24 14:03:12 -0300

    style(gui): use light green for all implemented skills

--- src/main/java/com/rpgcustom/habilidadesplus/gui/MMOMenu.java
+++ src/main/java/com/rpgcustom/habilidadesplus/gui/MMOMenu.java
@@ -462,7 +462,7 @@ public final class MMOMenu {
             lore.add("&cVocê precisa do nível " + power.level() + " para desbloquear!");
         }
 
-        String color = !power.implemented() ? "&8" : unlocked ? "&a" : "&8";
+        String color = power.implemented() ? "&a" : "&8";
         return item(power.icon(), color + power.name() + " &8(" + skill.getDisplayName() + ")", lore);
     }
 
