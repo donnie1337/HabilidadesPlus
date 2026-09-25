636855b2607007076c221dd38c4663b921c40614
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-25 09:14:15 -0300

    Pass plugin context to Smelting ownership listener

--- src/main/java/com/rpgcustom/habilidadesplus/HabilidadesPlus.java
+++ src/main/java/com/rpgcustom/habilidadesplus/HabilidadesPlus.java
@@ -135,7 +135,7 @@ public final class HabilidadesPlus extends JavaPlugin {
         pm.registerEvents(new AcrobaticsListener(configManager, xpManager, dataManager), this);
         pm.registerEvents(new TamingListener(configManager, xpManager), this);
         pm.registerEvents(new AlchemyListener(configManager, xpManager), this);
-        pm.registerEvents(new ProductionListener(configManager, xpManager), this);
+        pm.registerEvents(new ProductionListener(this, configManager, xpManager), this);
         pm.registerEvents(new PlayerJoinQuitListener(dataManager, xpManager), this);
         pm.registerEvents(new MMOMenuListener(dataManager, levelingManager, configManager, top1SkillService), this);
     }
